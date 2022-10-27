577562977ed9278b566602fdd6665d68d26744d8
farrux2204 <ulugbekovfarrux0822@gmail.com>
2022-10-27 12:11:27 +0500

    08

--- file08.py
+++ file08.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    a=0
+    for i in data:
+        if i.isdigit():
+            if a<int(i):
+                a=int(i)
+
+    return a
 
 # Read data from file
+f=open("./txt_file/data08.txt")
+data=f.read()
+print(main(data))
