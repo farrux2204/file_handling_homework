e642732cd25a6c1048578ff29aa460e511fc89ac
farrux2204 <ulugbekovfarrux0822@gmail.com>
2022-10-27 10:43:32 +0500

    06

--- file06.py
+++ file06.py
@@ -2,9 +2,16 @@ def main(data:str):
     """
     The data is from the file. Find the each row length and return as list type.
     Args:
-        data: str
+        data: str   
     Returns:
         list: return answer
     """
-    
+    ls=data.split('\n')
+    ans=[]
+    for i in ls:
+        ans.append(len(str(i)))
+    return ans
 # Read data from file
+f=open("./txt_file/data06.txt")
+data=f.read()
+print(main(data))
